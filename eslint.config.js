ba20af7ee8bcb16bdfb3504fa607a1dfb4bd95b1
hammad-kamran <158271960+hammad-kamran@users.noreply.github.com>
2025-05-29 15:48:03 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "env", "node_modules", "supabase", "*.config.js"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
@@ -27,3 +27,4 @@ export default tseslint.config(
     },
   }
 );
+
